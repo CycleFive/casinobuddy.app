bcc15e5de8f2622b894ebc344e6194239ba4131c
Cycle Five <107008871+cycle-five@users.noreply.github.com>
2024-10-27 01:55:28 -0400

    cleanup some stuff (#15)
    * cleanup, refactor, and testings
    
    ---------
    
    Signed-off-by: Cycle Five <cycle.five@proton.me>

--- migrations/20241002185546_views.sql
+++ migrations/20241002185546_views.sql
@@ -6,12 +6,12 @@
 CREATE OR REPLACE VIEW user_spend_benefit AS SELECT
     user_id,
     SUM(cost) AS spend,
-    SUM(benefit) AS benefit, 
+    SUM(benefit) AS benefit,
     (SUM(benefit) - SUM(cost)) / SUM(cost) AS average_bonus,
     COUNT(*) AS transactions,
     MAX(created_at) AS last_transaction,
     MIN(created_at) AS first_transaction
-FROM "transaction" 
+FROM "transaction"
 GROUP BY user_id;
 
 -- Create a view to get the spend and benefit for each user for each casino
@@ -19,10 +19,17 @@ CREATE OR REPLACE VIEW user_casino_spend_benefit AS SELECT
     user_id,
     casino_id,
     SUM(cost) AS spend,
-    SUM(benefit) AS benefit, 
+    SUM(benefit) AS benefit,
     (SUM(benefit) - SUM(cost)) / SUM(cost) AS average_bonus,
     COUNT(*) AS transactions,
     MAX(created_at) AS last_transaction,
     MIN(created_at) AS first_transaction
 FROM "transaction"
 GROUP BY user_id, casino_id;
+
+-- Create a view to get the list of casinos and their names for each user
+CREATE OR REPLACE VIEW user_casino_name AS SELECT
+    user_id,
+    casino_id,
+    casino.name AS casino_name
+FROM (user_casino JOIN casino ON user_casino.casino_id = casino.id);
